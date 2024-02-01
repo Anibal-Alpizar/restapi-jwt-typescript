684e1f5426c07fac91e14422c1838b75b94f0bd0
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-01 01:40:37 -0600

    Add JWT authentication to user controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 import User, { IUser } from "../models/User";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+
+function createToken(user: IUser) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+
+    config.jwtSecret,
+    {
+      expiresIn: 86400, // 24 hours
+    }
+  );
+}
 
 export const signup = async (req: Request, res: Response) => {
   if (!req.body || !req.body.email || !req.body.password)
@@ -15,5 +31,14 @@ export const signup = async (req: Request, res: Response) => {
 };
 
 export const signin = async (req: Request, res: Response) => {
-  res.send("signin");
+  if (!req.body || !req.body.email || !req.body.password)
+    return res.status(400).send({ msg: "Email and password are required" });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send({ msg: "The user does not exist" });
+
+  const isMatch = await user.comparePassword(req.body.password);
+  if (isMatch) return res.send({ token: createToken(user) });
+
+  return res.status(400).send({ msg: "The email or password are incorrect" });
 };
