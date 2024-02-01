11581e94dda6d9bb2f38b4c1f3bee8e44c906b5b
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-01 01:33:10 -0600

    Add signup functionality to user controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
+import User, { IUser } from "../models/User";
 
 export const signup = async (req: Request, res: Response) => {
-  res.send("signup");
+  if (!req.body || !req.body.email || !req.body.password)
+    return res.status(400).send({ msg: "Email and password are required" });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send({ msg: "The user already exists" });
+
+  const newUser = new User(req.body);
+  await newUser.save();
+
+  return res.send({ newUser });
 };
 
 export const signin = async (req: Request, res: Response) => {
