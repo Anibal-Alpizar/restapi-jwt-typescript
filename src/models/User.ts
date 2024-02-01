95ac952305fc7a8ddd46adf9952074d4a4d383ec
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-01 01:28:49 -0600

    Add User model with password hashing

---
+++ src/models/User.ts
@@ -0,0 +1,38 @@
+import { model, Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre<IUser>("save", async function (next) {
+  const user = this as IUser;
+  if (!user.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default model<IUser>("User", userSchema);
