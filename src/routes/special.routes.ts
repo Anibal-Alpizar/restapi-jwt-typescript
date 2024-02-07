58c9fb7fa4f326804a72f9964b34a560bb82b42b
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-07 00:46:23 -0600

    Add API documentation for special route

--- src/routes/special.routes.ts
+++ src/routes/special.routes.ts
@@ -3,10 +3,31 @@ import passport from "passport";
 
 const router = Router();
 
+/**
+ * @api {get} /special Special route for authenticated users
+ * @apiName GetSpecial
+ * @apiGroup Special
+ *
+ *
+ * @apiSuccess {String} msg Success message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *      "msg": "Success"
+ *    }
+ *
+ * @apiError Unauthorized Unauthorized user 
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Not Found
+ *     Unauthorized
+ */
+
 router.get(
   "/special",
   passport.authenticate("jwt", { session: false }),
-  (req, res) => res.send("Success")
+  (req, res) => res.send({ msg: "Success" })
 );
 
 export default router;
