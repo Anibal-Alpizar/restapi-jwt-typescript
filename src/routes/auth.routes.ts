7a58206dedc5d41732287ebcc2eaca7b8ee0ac5e
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-06 23:22:14 -0600

    Add signup API documentation

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,33 @@ import { signin, signup } from "../controller/user.controller";
 
 const router = Router();
 
+/**
+ * @api {post} /signup Allow the user to signup
+ * @apiName PostSignup
+ * @apiGroup Auth
+ *
+ *
+ * @apiSuccess {String} _id id's user
+ * @apiSuccess {String} email email of the user
+ * @apiSuccess {String} password password of the user
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "_id": "5f3f3f3f3f3f3f3f3f3f3f3f",
+ *       "email": "test@gmail.com",
+ *       "password": "test"
+ *     }
+ *
+ * @apiError UserFound The user already exists
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Not Found
+ *     {
+ *       "msg": "User already exists"
+ *     }
+ */
+
 router.post("/signup", signup);
 
 router.post("/signin", signin);
